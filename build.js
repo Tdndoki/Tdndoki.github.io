5294ec8d3df45d2a224f2d3a7284da4ebc724be7
Tdndoki <3494329105@qq.com>
2024-03-29 15:40:20 +0800

    update bulltiin

--- build.js
+++ build.js
@@ -3,22 +3,33 @@ const fs = require("fs-extra");
 async function main() {
     let raw = (await fs.readFile("./index0.html")).toString();
     const songList = (await fs.readFile("./song-list.csv")).toString();
-    raw = raw.replace(`{{csv}}`, songList);
+    const bulletin = (await fs.readFile("./bulletin.txt")).toString().replace(/\n/g,'<br\/>');
+    const replaceList = [replaceSonglist, replaceBulletin];
+    raw = replaceMultiple(raw, replaceList);
     const indexTPL = raw.replace("./res/bg-hd.jpg", "https://ooo.0x0.ooo/2024/03/08/OyjoKN.jpg")
         .replace("./res/bg.jpg", "https://ooo.0x0.ooo/2024/03/08/OyjqSC.jpg");
     const indexTPLSingle = raw;
 
     await fs.writeFile("./index-tpl.html", indexTPL);
     await fs.writeFile("./index-tpl-single.html", indexTPLSingle);
-    function replace(input){
+    function replaceSonglist(input) {
         return input.replace(/<pre [\s\S]+?pre>/, `<pre id="csv" style="display:none;">${songList}</pre>`);
     }
-    await fileReplace("./index.html",replace);
-    await fileReplace("./index-single.html",replace);
+    function replaceBulletin(input) {
+        return input.replace(/<div id="intro-right">[\s\S]+?div>/, `<div id="intro-right">${bulletin}</div>`);
+    }
+    await fileReplace("./index.html", replaceList);
+    await fileReplace("./index-single.html", replaceList);
 }
-async function fileReplace(filename, cb) {
+async function fileReplace(filename, cbs) {
     let raw = (await fs.readFile(filename)).toString();
-    const result = cb(raw);
-    await fs.writeFile(filename, result);
+    raw = replaceMultiple(raw, cbs);
+    await fs.writeFile(filename, raw);
+}
+function replaceMultiple(input, cbs) {
+    cbs.forEach((cb) => {
+        input = cb(input);
+    });
+    return input;
 }
 main();
